fffd7b32ef8bdef106a58b2bff65ffb2e4ad7ae1
pedro.compasso <pedroarthur06@alu.ufc.br>
2021-08-17 11:08:56 -0300

    feat: configurate splash screen

--- devquiz/lib/core/app_widget.dart
+++ devquiz/lib/core/app_widget.dart
@@ -1,4 +1,3 @@
-import 'package:DevQuiz/home/home_page.dart';
 import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +6,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
