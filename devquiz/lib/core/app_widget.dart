ac7b053d9c299bc491e978fa5e5906a033f3b8c4
pedro.compasso <pedroarthur06@alu.ufc.br>
2021-08-17 09:47:37 -0300

    config: initialize flutter project

---
+++ devquiz/lib/core/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:DevQuiz/home/home_page.dart';
+import 'package:DevQuiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: HomePage(),
+    );
+  }
+}
