036c83160622aaa072f965a06a3a2a4d9dbf8faa
pedro.compasso <pedroarthur06@alu.ufc.br>
2021-08-18 23:25:35 -0300

    feat: adding appbar

--- devquiz/lib/core/app_widget.dart
+++ devquiz/lib/core/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/home/home_page.dart';
 import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -6,7 +7,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
---
+++ devquiz/lib/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
