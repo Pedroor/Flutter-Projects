dc9f71963359a28ceea659adc8e02d5449c11429
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-05-04 13:52:54 -0300

    feat: create login screen

--- payflow/lib/main.dart
+++ payflow/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
@@ -15,7 +16,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary
       ),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
